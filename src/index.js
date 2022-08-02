ac981e50d0155485108b88b91206321dbefc0339
Art <pytagpro@gmail.com>
2022-08-02 07:53:35 +0500

    feat: add solve for hundred 00.

--- src/index.js
+++ src/index.js
@@ -17,15 +17,44 @@ let numbers = {
     15: "fifteen",
     20: "twenty",
     30: "thirty",
+    40: "forty",
+    50: "fifty",
+    80: "eighty",
 };
 
-function toReadable(number) {
-    if (number <= 15 || number == 20 || number == 30) {
-        console.log(numbers[number]);
+module.exports = function toReadable(number) {
+    const hundred = "hundred";
+    if (
+        number <= 15 ||
+        number == 20 ||
+        number == 30 ||
+        number == 40 ||
+        number == 50 ||
+        number == 80
+    ) {
         return numbers[number];
     } else if (number > 15 && number < 20) {
         return numbers[String(number % 10)] + "teen";
     } else if (number > 20 && number < 30) {
         return String(numbers[20]) + " " + numbers[String(number % 10)];
+    } else if (number > 30 && number < 40) {
+        return String(numbers[30]) + " " + numbers[String(number % 10)];
+    } else if (number > 40 && number < 50) {
+        return String(numbers[40]) + " " + numbers[String(number % 10)];
+    } else if (number > 50 && number < 60) {
+        return String(numbers[50]) + " " + numbers[String(number % 10)];
+    } else if (number > 80 && number < 90) {
+        return String(numbers[80]) + " " + numbers[String(number % 10)];
+    } else if (number > 60 && number < 100) {
+        return (
+            String(numbers[Math.floor(number / 10)]) +
+            "ty" +
+            " " +
+            numbers[String(number % 10)]
+        );
+    } else if (number >= 100 && number < 1000) {
+        if (number % 100 == 0) {
+            return String(numbers[Math.floor(number / 100)]) + " " + hundred;
+        }
     }
-}
+};
